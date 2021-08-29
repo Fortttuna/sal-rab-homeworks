ca0caf04f49cb5a3f773bace779dd9422c0e85d1
Your Name <you@example.com>
2021-08-29 22:21:33 +0300

    task2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,11 +8,11 @@ function calcShipping(sum, min, shipping) {
     let shippingSum; // создайте переменную shippingSum
 
 if (productsSum == 0 || productsSum >= freeShippingMinSum) {
-    shippingSum = 0;
+shippingSum = 0;
 } // если productsSum равно 0 или если productsSum Больше или равна freeShippingMinSum, то shippingSum присвоить значение 0
   
 } else if ((productsSum > 0) && (productsSum < freeShippingMinSum))  {
-    shippingSum = shippingPrice;
+shippingSum = shippingPrice;
 }// если productsSum больше 0 и меньше freeShippingMinSum,то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
