9fb70d6eb7f51f6c4b5514b83ed9b252884e1d25
Your Name <you@example.com>
2021-09-05 10:24:31 +0300

    task3

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,12 +3,12 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    // product - объект вида {id: id, title: title, count: count, price: price}
+    let product = {id: id, title: title, count: count, price: price}// product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
-    // price - цена
-    // count - количество
+    let price = price; // price - цена
+    let count = count; // count - количество
 
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
