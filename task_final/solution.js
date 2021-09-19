47e479cf2a1d2567e7bbd2a294ab4b81cfd1a5f2
Your Name <you@example.com>
2021-09-19 18:49:03 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -44,7 +44,7 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53";
+    data.order.address = address;
     data.order.sum = sum;
 
     data.client = name + " " + phone;
